50a438d65910246d937e93a2e9d33119959f28d0
Rick Belson <belsonrj@aol.com>
2020-08-05 21:16:04 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -13,3 +13,52 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const initialize = () => {
+  fetch('http://localhost:3000/toys')
+    .then(resp => resp.json())
+    .then(toys => toys.forEach(showToy))
+}
+
+const showToy = toy => {
+  let toyNew = toy
+  let toyCard = document.createElement('div')
+  toyCard.className = 'card'
+  toyCard.innerHTML = `
+  <h2>${toyNew.name}</h2>
+  <img src=${toyNew.image} class="toy-avatar" />`
+  let likesP = document.createElement('p')
+  likesP.innerText = `${toyNew.likes} Likes`
+  toyCard.append(likesP)
+  let likeBtn = document.createElement('button')
+  likeBtn.className = 'like-btn'
+  likeBtn.innerText = 'Like <3'
+  likeBtn.addEventListener('click', e => increaseLikes(e, toyNew))
+  toyCard.append(likeBtn)
+  toyCollection.append(toyCard)
+}
+
+const increaseLikes = (e, toy) => {
+  let toyLikes = toy.likes
+  let toyId = toy.id
+  return fetch(`http://localhost:3000/toys/${toyId}`, {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json'
+    },
+    body: JSON.stringify({ likes: ++toyLikes })
+  })
+    .then(resp => resp.json())
+    .then(showLikes(e))
+}
+
+const showLikes = e => {
+  e.preventDefault()
+  // let num =  e.target.parentElement.children[2].innerText
+  let newNum =
+    parseInt(e.target.parentElement.children[2].innerText.split(' ')[0]) + 1
+  e.target.parentElement.children[2].innerText = `${newNum} Likes`
+}
+
+initialize()
